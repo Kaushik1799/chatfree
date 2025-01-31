efb038d1586ea2d10141293ba208555680cb2f07
Kaushik1799 <tupa21222.ad@rmkec.ac.in>
2025-01-31 16:52:24 +0530

    itnial commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: 'mychat-f1065.firebaseapp.com',
-    projectId: 'mychat-f1065',
-    storageBucket: 'mychat-f1065.appspot.com',
-    messagingSenderId: '503625873226',
-    appId: '1:503625873226:web:9f02693f9268c15c76b3b3',
-}
+    apiKey: "AIzaSyBo2oJWqeBjCdPnaFUZtx2AoQeZ4VFAULA",
+    authDomain: "chatfree-2b9c9.firebaseapp.com",
+    projectId: "chatfree-2b9c9",
+    storageBucket: "chatfree-2b9c9.firebasestorage.app",
+    messagingSenderId: "1028156914965",
+    appId: "1:1028156914965:web:634931a0ec17f7c7543d1c"
+  };
 
 const app = initializeApp(firebaseConfig)
 
